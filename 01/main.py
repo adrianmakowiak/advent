917b6a8d0f836c4d48be15bf72a35df2aa041475
adrianmakowiak <adrian.makowiak@gmail.com>
2021-12-05 00:12:10 +0100

    01.02

--- 01/main.py
+++ 01/main.py
@@ -10,6 +10,19 @@ def main():
         print(counter)
 
 
+def main2():
+    with open('data.txt', 'r') as f:
+        previous = None
+        counter = 0
+        values = [int(line) for line in f.readlines()]
+        for i in range(len(values) - 2):
+            current = values[i] + values[i+1] + values[i+2]
+            if previous and current > previous:
+                counter += 1
+            previous = current
+        print(counter)
+
+
 if __name__ == '__main__':
-    main()
+    main2()
 
