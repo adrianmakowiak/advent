1cc71e5fd6ef7b27572a285cc0a738e5507a1c7f
adrianmakowiak <adrian.makowiak@gmail.com>
2021-12-05 01:52:29 +0100

    03.02

--- 03/main.py
+++ 03/main.py
@@ -1,18 +1,36 @@
 def main():
     with open('data.txt', 'r') as f:
         values = f.readlines()
-        most_common = [0] * (len(values[0]) - 1)
-        for val in values:
-            v = val.strip()
-            for i, single_bit in enumerate(v):
-                most_common[i] = most_common[i] + 1 if single_bit == "1" else most_common[i] - 1
+        values_copy = values.copy()
+        dominating_bit_counter, ones, zeros = 0, [], []
+        for index in range(len(values[0].strip())):
+            for val in values:
+                dominating_bit_counter = dominating_bit_counter + 1 if val[index] == "1" else dominating_bit_counter - 1
+                if val[index] == '1':
+                    ones.append(val)
+                else:
+                    zeros.append(val)
+            values = ones if dominating_bit_counter >= 0 else zeros
+            dominating_bit_counter, ones, zeros = 0, [], []
 
-        gamma_bin = ["1" if x > 0 else "0" for x in most_common]
-        epsilon_bin = ["0" if x > 0 else "1" for x in most_common]
+        oxygen = int(values[0].strip(), 2)
 
-        gamma = int(''.join(gamma_bin), 2)
-        epsilon = int(''.join(epsilon_bin), 2)
-        print(gamma * epsilon)
+        dominating_bit_counter, ones, zeros = 0, [], []
+        for index in range(len(values_copy[0].strip())):
+            for val in values_copy:
+                dominating_bit_counter = dominating_bit_counter + 1 if val[index] == "1" else dominating_bit_counter - 1
+                if val[index] == '1':
+                    ones.append(val)
+                else:
+                    zeros.append(val)
+            values_copy = zeros if dominating_bit_counter >= 0 else ones
+            if len(values_copy) == 1:
+                break
+            dominating_bit_counter, ones, zeros = 0, [], []
+
+        co2 = int(values_copy[0].strip(), 2)
+
+        print(oxygen * co2)
 
 
 if __name__ == '__main__':
