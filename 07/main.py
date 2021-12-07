02cd5360f8ab797f650991328857e5249d9ab268
adrianmakowiak <adrian.makowiak@gmail.com>
2021-12-07 11:29:45 +0100

    07.02

--- 07/main.py
+++ 07/main.py
@@ -1,17 +1,27 @@
-from statistics import median
+from statistics import mean, median
+import numpy as np
+import math
 
 
 def main():
     with open('data.txt', 'r') as f:
         data = f.readline()
         crabs_position = [int(pos) for pos in data.strip().split(',')]
-        average = median(crabs_position)
+        average = round(mean(crabs_position))
+        result_median = int(median(crabs_position))
+        last_result = math.inf
 
-        print(average)
-        fuel_used = 0
-        for pos in crabs_position:
-            fuel_used += abs(average - pos)
-        print(fuel_used)
+        for value in range(average, result_median, -1):
+            fuel_used = 0
+            for pos in crabs_position:
+                steps = abs(value - pos)
+                for i in range(steps):
+                    fuel_used += i + 1
+            if last_result < fuel_used:
+                print(last_result)
+                return
+            else:
+                last_result = fuel_used
 
 
 if __name__ == '__main__':
