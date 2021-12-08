ea5c135c7c36dfebf282f9059e05b29c94d126e0
adrianmakowiak <adrian.makowiak@gmail.com>
2021-12-08 18:27:43 +0100

    06.02

--- 06/main.py
+++ 06/main.py
@@ -1,49 +1,70 @@
-import numpy as np
+init_value = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+}
+
 class FishSimulation:
-    fish_school = []
-    days_passed = 0
+    fish_school = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+    }
+    fish_copy = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+    }
 
     def __init__(self, fish_lives):
         for live in fish_lives:
-            self.fish_school.append(Fish(live))
+            self.fish_school[int(live)] += 1
 
     def start(self, days):
         for i in range(days):
-            for fish in self.fish_school:
-                if fish.day_pass():
-                    self.fish_school.append(Fish(9))
-            # print(f'Day: {i+1} {[f.days_to_reproduce for f in self.fish_school]}')
-            print(f'Days: {i}, fish: {len(self.fish_school)}')
-        print(f"fish: {len(self.fish_school)}")
-
-
-class Fish:
-    days_to_reproduce = 0
-
-    def __init__(self, days):
-        self.days_to_reproduce = int(days)
-
-    def day_pass(self) -> bool:
-        if self.days_to_reproduce == 0:
-            self.days_to_reproduce = 6
-            return True
-        self.days_to_reproduce -= 1
-        return False
+            for key, number_fish in self.fish_school.items():
+                if key == 0:
+                    self.fish_copy[6] += number_fish
+                    self.fish_copy[8] += number_fish
+                elif key in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+                    self.fish_copy[key - 1] += number_fish
+            self.fish_school = self.fish_copy
+            self.fish_copy = init_value.copy()
+            print(self.fish_copy)
+            # print(f'Days: {i}, fish: {len(self.fish_school)}')
+        print(f"fish: {sum(self.fish_school.values())}")
 
 
 def main():
     with open('data.txt', 'r') as f:
         data = f.readline()
         fish_lives = data.strip().split(',')
-        rates = []
-        fish_sim = FishSimulation([6])
-        for i in range(100):
-            fish_sim.start(1)
-            rates.append(len(fish_sim.fish_school))
-            # print(f'## {len(fish_sim.fish_school)}')
 
-        growth_rate = np.exp(np.diff(np.log(rates))) - 1
-        print(growth_rate)
-        print(np.average(growth_rate))
+        fish_sim = FishSimulation(fish_lives)
+
+        fish_sim.start(256)
+        print(fish_sim.fish_school)
+
+
 if __name__ == '__main__':
     main()
