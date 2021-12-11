cfd3c973a0d2bda3a777f346f44592d34b10de2c
adrianmakowiak <adrian.makowiak@gmail.com>
2021-12-11 16:49:34 +0100

    10.02

--- 10/main.py
+++ 10/main.py
@@ -1,4 +1,6 @@
 # from functools import
+import math
+
 
 def main():
     with open('data.txt', 'r') as f:
@@ -18,22 +20,36 @@ def main():
             '>': 25137,
         }
 
+        otp = {
+            '(': 1,
+            '[': 2,
+            '{': 3,
+            '<': 4,
+        }
+
         points = 0
+        scores = []
         for line in data:
             string_line = line.strip()
             stash = []
+            broken = False
             for c in string_line:
-                print(stash)
                 if c in opening:
                     stash.append(c)
                 elif c in closing:
                     if otc[stash[-1]] != c:
-                        print(f'expected: {otc[stash[-1]]}, received: {c}')
+                        # print(f'expected: {otc[stash[-1]]}, received: {c}')
                         points += ctp[c]
+                        broken = True
                         break
                     else:
                         stash.pop()
-        print(points)
+            if not broken:
+                score = 0
+                for p in reversed(stash):
+                    score = score * 5 + otp[p]
+                scores.append(score)
+        print(sorted(scores)[math.floor(len(scores)/2)])
 
 
 if __name__ == '__main__':
