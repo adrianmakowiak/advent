6c96a289a1e977947cea499c968a8bf6861b29c9
adrianmakowiak <adrian.makowiak@gmail.com>
2021-12-13 22:48:50 +0100

    12.02

--- 12/main.py
+++ 12/main.py
@@ -1,4 +1,4 @@
-
+from collections import Counter
 def main():
     nodes = {}
 
@@ -22,16 +22,25 @@ def main():
 
 
 PATHS = []
+
+
 def check_sub_tree(nodes, current_node, so_far):
     if current_node == 'end':
             print(f'Congrats, you found:{so_far}')
             PATHS.append(so_far)
             return
     for n in nodes[current_node]:
-        if n in so_far and n.islower():
+        counted = Counter(so_far)
+        already_has_two_small = False
+        for cave, occurrences in counted.items():
+            if cave.islower() and occurrences > 1:
+                already_has_two_small = True
+        if n.islower() and n in counted and (counted[n] > 1 or (counted[n] == 1 and already_has_two_small)):
             continue
+
         new_path = [*so_far, n]
         check_sub_tree(nodes, n, new_path)
+
 
 def check_tree(nodes):
     start_nodes = nodes['start']
