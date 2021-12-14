8105f2c42cdaac560198ea66cd0bdcee1eb7bf8f
adrianmakowiak <adrian.makowiak@gmail.com>
2021-12-14 23:59:04 +0100

    13.02

--- 13/main.py
+++ 13/main.py
@@ -1,4 +1,6 @@
 from collections import Counter
+from pprint import pprint
+
 def main():
     with open('data.txt', 'r') as f:
         data = f.readlines()
@@ -16,36 +18,46 @@ def main():
         print(point_set)
         inst = instructions[0]
         print(len(point_set))
-        new_set = set()
-        if inst[0] == 'x':
-            for point in point_set:
-                a, b = point.split('-')
-                x = int(a)
-                y = int(b)
-                if x > inst[1]:
-                    new_set.add(f'{inst[1] - (x - inst[1])}-{y}')
+        for inst in instructions:
+            new_set = set()
+            if inst[0] == 'x':
+                for point in point_set:
+                    print(point)
+                    a, b = point.split('-')
+                    x = int(a)
+                    y = int(b)
+                    if x > inst[1]:
+                        new_set.add(f'{inst[1] - (x - inst[1])}-{y}')
+                    else:
+                        new_set.add(point)
+            elif inst[0] == 'y':
+                for point in point_set:
+                    print(point)
+                    a, b = point.split('-')
+                    x = int(a)
+                    y = int(b)
+                    if y > inst[1]:
+                        new_set.add(f'{x}-{inst[1] - (y - inst[1])}')
+                    else:
+                        new_set.add(point)
+            point_set = new_set.copy()
+            print(point_set)
+
+        print(point_set)
+        print(len(point_set))
+
+        result = ''
+
+        tmp = 0
+        for i in range(38):
+            for j in range(6):
+                if f'{i}-{j}' in point_set:
+                    result += '#'
                 else:
-                    new_set.add(point)
+                    result += '.'
 
-        print(len(new_set))
-        # elif inst[0] == 'y':
-        #     for x, y in points_dict.items():
-        #         if y > inst[1]:
-        #             dict_step[inst[1] - (y - inst[1])] = y
-        #         else:
-        #             dict_step[x] = y
-        #
+            result += '\n'
 
-        # print(points)
-        # print(instructions)
-        # print(points_dict)
-        # for inst in instructions:
-        # #     print(inst)
-        #
-        # dict_step = {}
-
-        # print(len(points_dict))
-        # print(len(dict_step))
 
 if __name__ == '__main__':
     main()
